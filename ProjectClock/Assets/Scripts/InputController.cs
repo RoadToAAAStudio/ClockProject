15b453a4322738312f4330b37848a26c523b98f3
JackAlone96 <giacalone.giovanni96@gmail.com>
2024-05-17 22:06:25 +0200

    Implemented GameOver

--- ProjectClock/Assets/Scripts/InputController.cs
+++ ProjectClock/Assets/Scripts/InputController.cs
@@ -2,18 +2,25 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class InputController : MonoBehaviour
 {
     public List<GameObject> hands = new List<GameObject>();
     private int index = 0;
     public float precision = -0.5f;
-    float dotProduct = 0;
+    public float dotProduct = 0;
     private float oldProduct = float.NegativeInfinity;
+    public bool canLose = false;
 
     private void Awake()
     {
         hands[0].GetComponent<Clock>().enabled = true;
+    }
+
+    private void Start()
+    {
+        EventManagerOneParam<GameObject>.Instance.TriggerEvent("onNewClock", hands[index]);
     }
 
     // Update is called once per frame
@@ -37,10 +44,22 @@ public class InputController : MonoBehaviour
                     }
                     
                     hands[index].GetComponent<Clock>().enabled = true;
+                    EventManagerOneParam<GameObject>.Instance.TriggerEvent("onNewClock", hands[index]);
                 }
             }
         }
+
         oldProduct = dotProduct;
+        
+        if (dotProduct < 0)
+        {
+            canLose = true;
+        }
+
+        if (canLose && oldProduct < dotProduct)
+        {
+            GameOver();
+        }
     }
 
     //private bool Check()
@@ -61,8 +80,14 @@ public class InputController : MonoBehaviour
         {
             return true;
         }
-        
+
+        GameOver();
         return false;
+    }
+
+    private void GameOver()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
 }
